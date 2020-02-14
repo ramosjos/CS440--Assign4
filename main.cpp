16ebc0561a41aac7ddff094497abf24be820634b
ramosjos <ramosjos@oregonstate.edu>
2020-02-14 12:01:20 -0800

    tokenizer

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream> 
+#include <string>
 #include <fstream>
+#include <sstream>
+#include <vector>
+#include <stdlib.h>
+
+int runNumber = 0;
 
 using namespace std;
 
@@ -17,11 +23,47 @@ struct Emp {
 	double salary;
 };
 
-void readRelation(string fileName){
-	ifstream infile("ndndnd");
-	ofstream outfile("tempSorted.csv");
+void readRelation(){
+	int index = 0;
+	struct Dept deptStruct;
+	vector<Dept> runVector;
+	string line;	
+	string delimiter = ",";
+	ifstream infile ("Dept.csv");
+	if(infile.is_open()){
+		while(getline(infile, line)){
+			index++;
+			vector<string> holdTokens;
+			size_t pos = 0;
+			string token;
+			while ((pos = line.find(delimiter)) != std::string::npos) {
+				token = line.substr(0, pos);
+				holdTokens.push_back(token);
+				line.erase(0, pos + delimiter.length());
+			}
+			token = line.substr(0, pos);
+			holdTokens.push_back(token);
+			deptStruct.did = atoi(holdTokens[0].c_str());
+			deptStruct.dname = holdTokens[1];
+			deptStruct.budget = atof(holdTokens[2].c_str());
+			deptStruct.managerid = atoi(holdTokens[3].c_str());
+			runVector.push_back(deptStruct);
+			if(index % 22 == 0){
+				// call writeToFile();
+				index = 0;
+				runVector.clear();
+				runNumber++;
+			}
+		}
+	}
 }
-
+/*
+   stringstream ss;
+   ss << runNumber;
+   string tempFile = fileName + "_runNumber_" + ss.str();
+   ofstream
+   */
 int main(){
+	readRelation();
 	return 0;
 }
