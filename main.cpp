1217da51d290ecb6d9d3aa6931d5bad0c95070f4
ramosjos <ramosjos@oregonstate.edu>
2020-02-13 20:52:43 -0800

    Starter code

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 #include <iostream> 
+#include <fstream>
 
 using namespace std;
 
-int main(){
+struct Dept {
+	int did;
+	string dname;
+	double budget;
+	int managerid;
+};
 
+struct Emp {
+	int eid;
+	string ename;
+	int age;
+	double salary;
+};
+
+void readRelation(string fileName){
+	ifstream infile("ndndnd");
+	ofstream outfile("tempSorted.csv");
+}
+
+int main(){
 	return 0;
 }
